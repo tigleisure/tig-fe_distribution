98d4e93a156527caa50bb493c8db066269c3e62c
eunsu <songess@naver.com>
2024-06-29 16:06:19 +0900

    [Style] : colorStyle 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,24 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      extend: {
+        colors: {
+          'primary_orange1': '#FF6B00',
+          'primary_orange2': '#FFF0D3',
+          'secondary_green1' : '#30FF6B',
+          'secondary_green2': '#D5FFE2',
+          'white': '#FFFFFF',
+          'grey1': '#F8F9FA',
+          'grey2': '#F5F6F7',
+          'grey3': '#CED3D6',
+          'grey4': '#A9AFB3',
+          'grey5': '#878D91',
+          'grey6': '#4D5256',
+          'grey7': '#292A2B',
+          'status_red1': '#F45858',
+          'black': '#000000',
+        },
       },
-    },
   },
   plugins: [],
 };
