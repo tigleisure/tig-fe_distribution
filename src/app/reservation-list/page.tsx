b75c3057b8dd263f98c87af00db5763f1ec4d340
Programming-Seungwan <swsj4480@gmail.com>
2024-07-05 01:18:52 +0900

    [Fix]: mypage label 간극 문제 해결
    1. 동적으로 결정은 못해서 clsx 분기를 쳐줘야 함

--- src/app/reservation-list/page.tsx
+++ src/app/reservation-list/page.tsx
@@ -1,10 +1,11 @@
-import NavBar from "@components/all/NavBar/NavBar";
-
+import NavBar from '@components/all/NavBar/NavBar';
+import HistoryHead from '@components/reservation-list/HistoryHead';
 
 export default function page() {
   return (
     <div>
       This is reservation-list page
+      <HistoryHead totalCount={5} inProgressCount={3} completedCount={2} />
       <NavBar />
     </div>
   );
---
+++ src/components/reservation-list/HistoryHead.tsx
@@ -0,0 +1,79 @@
+// 해당 컴포넌트는 전체, 진행중, 종료된을 보여주는 UI이기에 내역의 대가리라는 의미에서 Head라고 명명함
+'use client';
+import { useState } from 'react';
+import { cn } from '@utils/cn';
+interface reservationHistoryCountProps {
+  totalCount: number;
+  inProgressCount: number;
+  completedCount: number;
+}
+
+interface historyItemProps {
+  label: string;
+  count: number;
+  isFocused: boolean;
+  handleReservationState: (st: string) => void;
+}
+
+function HistoryHeadItem({
+  label,
+  count,
+  isFocused,
+  handleReservationState,
+}: historyItemProps) {
+  return (
+    <div
+      className="w-fit h-4 flex justify-between items-center gap-x-1 hover:cursor-pointer"
+      onClick={() => handleReservationState(label)}
+    >
+      <span
+        className={cn('title2', {
+          'text-grey7': isFocused,
+          'text-grey4': !isFocused,
+        })}
+      >
+        {label}
+      </span>
+      <span
+        className={cn('title2', {
+          'text-primary_orange1': isFocused,
+          'text-grey4': !isFocused,
+        })}
+      >
+        {count}
+      </span>
+    </div>
+  );
+}
+
+export default function HistoryHead({
+  totalCount,
+  inProgressCount,
+  completedCount,
+}: reservationHistoryCountProps) {
+  // 현재 각 아이템을 클릭하며 테스트하기 위한 용도이고, 추후에 전역 상태로 바뀌어 관련 다른 UI과 조화될 수 있음
+  const [reservationState, setReservationState] = useState<string>('전체');
+
+  return (
+    <div className="w-full h-[49px] px-5 flex justify-start items-center gap-x-5">
+      <HistoryHeadItem
+        label="전체"
+        count={totalCount}
+        isFocused={reservationState === '전체'}
+        handleReservationState={setReservationState}
+      />
+      <HistoryHeadItem
+        label="진행중"
+        count={inProgressCount}
+        isFocused={reservationState === '진행중'}
+        handleReservationState={setReservationState}
+      />
+      <HistoryHeadItem
+        label="종료된"
+        count={completedCount}
+        isFocused={reservationState === '종료된'}
+        handleReservationState={setReservationState}
+      />
+    </div>
+  );
+}
