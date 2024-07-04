077a99f6bd8d7e7fc5da65cc70242cbc250d0a02
eunsu <songess@naver.com>
2024-07-04 15:02:55 +0900

    [Feat] : Tabs 필요한 페이지에 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import NavBar from '@components/all/NavBar';
+import Tabs from '@components/all/Tabs/Tabs';
+import { homeleisureArray } from '@constant/constant';
 
 export default function Home() {
+  const homeArray = homeleisureArray;
   return (
     <div>
+      <Tabs tabArray={homeArray} from="home" />
       <NavBar />
     </div>
   );
