9d39acae23c51b9dcd8d1a814ddc486c4e71c335
eunsu <songess@naver.com>
2024-07-04 22:17:51 +0900

    [Fix] : 브랜치 충돌 해결

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,8 +2,11 @@ import Header from '@components/all/Header';
 import SearchInput from '@components/all/SearchInput';
 
 export default function page() {
-  return <div>
-    <Header buttonType="close" isCenter={true} title="검색하기"/>
-    <SearchInput placeholder="위치나 장소검색" />
-  </div>;
+  return (
+    <div>
+      <Header buttonType="close" isCenter={true} title="검색하기" />
+      <SearchInput placeholder="위치나 장소검색" />
+      merge
+    </div>
+  );
 }
