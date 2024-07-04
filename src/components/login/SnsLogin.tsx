aa5036ae8dd73c013a31c12a9dc62d17c0c9692e
Programming-Seungwan <swsj4480@gmail.com>
2024-07-04 23:33:11 +0900

    [Refactor] : login, mypage 컴포넌트 리팩터링
    1. cn을 활용하여 코드 가독성 개선
    2. /search/result에서의 import문, 결제 완료 페이지 텍스트 수정

--- src/components/login/SnsLogin.tsx
+++ src/components/login/SnsLogin.tsx
@@ -1,23 +1,44 @@
 import KakaoLogoSVG from '@public/svg/kakaoLogo.svg';
 import GoogleLogoSVG from '@public/svg/googleLogo.svg';
+import { cn } from '@utils/cn';
+
+interface SnsLoginProps {
+  companyName: string;
+}
+
+function SnsLoginComponent({ companyName }: SnsLoginProps) {
+  return (
+    <div
+      id="kakaoLogo"
+      className={cn(
+        'w-full h-[47px] flex justify-center items-center rounded-[10px] hover:cursor-pointer',
+        {
+          'gap-x-[10px] bg-[#FEE500]': companyName === 'kakao',
+          'gap-x-[15px] bg-white border-[1px] border-grey3':
+            companyName === 'google',
+          '': companyName === 'kakao',
+        }
+      )}
+    >
+      {companyName === 'kakao' && <KakaoLogoSVG />}
+      {companyName === 'google' && <GoogleLogoSVG />}
+      <span className="text-grey7 body2">
+        {companyName === 'kakao'
+          ? '카카오'
+          : companyName === 'google'
+          ? 'google'
+          : null}
+        로 로그인
+      </span>
+    </div>
+  );
+}
 
 export default function SnsLogin() {
   return (
     <div className="w-full h-fit flex flex-col justify-between items-center gap-y-[10px]">
-      <div
-        id="kakaoLogo"
-        className="w-full h-[47px] flex justify-center items-center gap-x-[10px] bg-[#FEE500] rounded-[10px] hover:cursor-pointer"
-      >
-        <KakaoLogoSVG />
-        <span className="text-grey7 body2">카카오로 로그인</span>
-      </div>
-      <div
-        id="googleLogo"
-        className="w-full h-[47px] flex justify-center items-center gap-x-[15px] bg-white rounded-[10px] border-[1px] border-grey3 hover:cursor-pointer"
-      >
-        <GoogleLogoSVG />
-        <span className="text-grey7 body2">Google로 로그인</span>
-      </div>
+      <SnsLoginComponent companyName="kakao" />
+      <SnsLoginComponent companyName="google" />
     </div>
   );
 }
--- src/components/mypage/ProfileInformation.tsx
+++ src/components/mypage/ProfileInformation.tsx
@@ -1,69 +1,53 @@
+import { ProfileInformationItemProps } from 'types/mypage/MyPageTypes';
+import ProfileInformationItem from './ProfileInformationItem';
+
 export default function ProfileInformation() {
+  const profileData: ProfileInformationItemProps[] = [
+    {
+      wholeGap: 156,
+      labelGap: 63,
+      labelName: '이름',
+      placeholderName: '김티그',
+      placeholderColor: 'grey7',
+      isButtonBorder: true,
+      buttonText: '변경',
+    },
+    {
+      wholeGap: 59,
+      labelGap: 33,
+      labelName: '휴대폰번호',
+      placeholderName: '휴대폰번호를 입력해주세요',
+      placeholderColor: 'grey3',
+      isButtonBorder: false,
+      buttonText: '변경',
+    },
+    {
+      wholeGap: 108,
+      labelGap: 53,
+      labelName: '이메일',
+      placeholderName: 'tig@naver.com',
+      placeholderColor: 'grey7',
+      isButtonBorder: true,
+      buttonText: '변경',
+    },
+  ];
   return (
     <div
       id="profile-info-container"
       className="w-full h-fit flex flex-col gap-y-4"
     >
-      <div
-        id="nameDiv"
-        className="w-full h-fit flex justify-between items-center gap-x-[156px]"
-      >
-        <div
-          id="labelAndName"
-          className="flex justify-between items-center gap-x-[63px]"
-        >
-          <span id="nameLabel" className="caption2 text-grey5">
-            이름
-          </span>
-          {/* 해당 이름은 추후에 변경 가능 */}
-          <span id="userName" className="body4 text-grey7">
-            김티그
-          </span>
-        </div>
-        <button className="w-fit h-fit rounded-md title4 text-grey7 bg-white shadow-mypageButton px-[14px] py-[8px]">
-          변경
-        </button>
-      </div>
-      <div
-        id="phoneNumberDiv"
-        className="w-full h-fit flex justify-between items-center gap-x-[59px]"
-      >
-        <div
-          id="labelAndPhoneNumber"
-          className="flex justify-between items-center gap-x-[33px]"
-        >
-          <span id="phoneNumberLabel" className="caption2 text-grey5">
-            휴대폰번호
-          </span>
-          {/* 해당 이름은 추후에 변경 가능 */}
-          <span id="userPhoneNumber" className="body4 text-grey3">
-            휴대폰번호를 입력해주세요
-          </span>
-        </div>
-        <button className="w-fit h-fit rounded-md title4 text-white bg-primary_orange1 px-[14px] py-[8px]">
-          변경
-        </button>
-      </div>
-      <div
-        id="emailDiv"
-        className="w-full h-fit flex justify-between items-center gap-x-[108px]"
-      >
-        <div
-          id="labelAndEmail"
-          className="flex justify-between items-center gap-x-[53px]"
-        >
-          <span id="emailLabel" className="caption2 text-grey5">
-            이메일
-          </span>
-          {/* 해당 이름은 추후에 변경 가능 */}
-          <span id="userEmail" className="body4 text-grey7">
-            tig@naver.com
-          </span>
-        </div>
-        <button className="w-fit h-fit rounded-md title4 text-grey7 bg-white shadow-mypageButton px-[14px] py-[8px]">
-          변경
-        </button>
-      </div>
+      {profileData.map((data, index) => (
+        <ProfileInformationItem
+          key={data.labelName}
+          wholeGap={data.wholeGap}
+          labelGap={data.labelGap}
+          labelName={data.labelName}
+          placeholderName={data.placeholderName}
+          placeholderColor={data.placeholderColor}
+          isButtonBorder={data.isButtonBorder}
+          buttonText={data.buttonText}
+        />
+      ))}
     </div>
   );
 }
