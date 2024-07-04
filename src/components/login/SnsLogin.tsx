ce00922fd7e8405148ccdce2c88c9a76046c1436
Programming-Seungwan <swsj4480@gmail.com>
2024-07-04 17:21:58 +0900

    [Feat] : mypage 기본 My 컴포넌트 완성
    1. button의 경우 border가 크기에 영향을 미칠 수 있으므로 box-shadow를 활용하여 구현
    2. boxShadow를 tailwind.config.ts 파일에서 생성

--- src/components/login/SnsLogin.tsx
+++ src/components/login/SnsLogin.tsx
@@ -6,14 +6,14 @@ export default function SnsLogin() {
     <div className="w-full h-fit flex flex-col justify-between items-center gap-y-[10px]">
       <div
         id="kakaoLogo"
-        className="w-full h-[47px] flex justify-center items-center gap-x-[10px] bg-[#FEE500] rounded-[10px]"
+        className="w-full h-[47px] flex justify-center items-center gap-x-[10px] bg-[#FEE500] rounded-[10px] hover:cursor-pointer"
       >
         <KakaoLogoSVG />
         <span className="text-grey7 body2">카카오로 로그인</span>
       </div>
       <div
         id="googleLogo"
-        className="w-full h-[47px] flex justify-center items-center gap-x-[15px] bg-white rounded-[10px] border-[1px] border-grey3"
+        className="w-full h-[47px] flex justify-center items-center gap-x-[15px] bg-white rounded-[10px] border-[1px] border-grey3 hover:cursor-pointer"
       >
         <GoogleLogoSVG />
         <span className="text-grey7 body2">Google로 로그인</span>
