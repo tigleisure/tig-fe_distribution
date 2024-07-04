1dae70168e22422bc641461f156290c415692591
Programming-Seungwan <swsj4480@gmail.com>
2024-07-04 16:21:18 +0900

    [Feat] : login 페이지 컴포넌트 완성

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,11 @@
 import NavBar from "@components/all/NavBar/NavBar";
+import Login from '@components/login/Login';
 
 export default function page() {
   return (
     <div>
       This is login page
+      <Login />
       <NavBar />
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-      extend: {
-        colors: {
-          'primary_orange1': '#FF6B00',
-          'primary_orange2': '#FFF0D3',
-          'secondary_green1' : '#30FF6B',
-          'secondary_green2': '#D5FFE2',
-          'white': '#FFFFFF',
-          'grey1': '#F8F9FA',
-          'grey2': '#F5F6F7',
-          'grey3': '#CED3D6',
-          'grey4': '#A9AFB3',
-          'grey5': '#878D91',
-          'grey6': '#4D5256',
-          'grey7': '#292A2B',
-          'status_red1': '#F45858',
-          'black': '#000000',
-        },
+    extend: {
+      colors: {
+        primary_orange1: '#FF6B00',
+        primary_orange2: '#FFF0D3',
+        secondary_green1: '#30FF6B',
+        secondary_green2: '#D5FFE2',
+        white: '#FFFFFF',
+        grey1: '#F8F9FA',
+        grey2: '#F5F6F7',
+        grey3: '#CED3D6',
+        grey4: '#A9AFB3',
+        grey5: '#878D91',
+        grey6: '#4D5256',
+        grey7: '#292A2B',
+        status_red1: '#F45858',
+        black: '#000000',
       },
+      width: {
+        loginWidth: 'calc(100% * (8/9))',
+      },
+    },
   },
   plugins: [],
 };
