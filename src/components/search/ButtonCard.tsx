1eee5bbb52804188505246475f5efd4bc25a57ed
eunsu <songess@naver.com>
2024-07-05 22:42:55 +0900

    [Feat] : search에서 검색버튼 클릭시 더미쿼리값을 가지고 result로 이동

--- src/components/search/ButtonCard.tsx
+++ src/components/search/ButtonCard.tsx
@@ -1,6 +1,19 @@
+'use client';
 import FullButton from '@components/all/FullButton';
+import { useRouter } from 'next/navigation';
+import querystring from 'querystring';
 
 export default function ButtonCard() {
+  const router = useRouter();
+  const handleSearch = () => {
+    const query = {
+      location: '신촌',
+      date: '24.05.17',
+      adultCount: '5',
+    };
+    const queryString = new URLSearchParams(query).toString();
+    router.push(`/search/result?${queryString}`);
+  };
   return (
     <section className="h-[78px] w-full flex justify-center items-center px-5 py-[14px] absolute bottom-0 bg-white shadow-absoluteButton">
       <FullButton
@@ -8,6 +21,7 @@ export default function ButtonCard() {
         color="white"
         content="검색"
         size="lg"
+        onClick={handleSearch}
       />
     </section>
   );
