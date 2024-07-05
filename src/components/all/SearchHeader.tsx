ad009021385fc69038c9c0e45d4f907df855d411
eunsu <songess@naver.com>
2024-07-05 15:59:31 +0900

    [Style] : SearchHeader에 absolute, bg-white 부여

--- src/components/all/SearchHeader.tsx
+++ src/components/all/SearchHeader.tsx
@@ -3,7 +3,7 @@ import SearchInput from './SearchInput';
 
 export default function SearchHeader() {
   return (
-    <section className="w-full py-[2px] px-4 flex items-center gap-4">
+    <section className="w-full py-[2px] px-4 bg-white flex items-center gap-4 absolute top-0">
       <TigSVG />
       <SearchInput placeholder="위치나 장소 검색" className="grow mr-1" />
     </section>
