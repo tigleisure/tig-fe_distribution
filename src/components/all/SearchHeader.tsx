9b5289094db887c78ad1b160ef09a1e68fa4e6db
eunsu <songess@naver.com>
2024-07-05 22:42:28 +0900

    [Refactor] : SearchHeader 컴포넌트 리팩토링

--- src/components/all/SearchHeader.tsx
+++ src/components/all/SearchHeader.tsx
@@ -1,11 +1,25 @@
 import TigSVG from '@public/svg/tig.svg';
 import SearchInput from './SearchInput';
+import { cn } from '@utils/cn';
 
-export default function SearchHeader() {
+interface SearchHeaderProps {
+  placeholder?: string;
+  result?: boolean;
+}
+
+export default function SearchHeader({
+  placeholder = '위치나 장소 검색',
+  result = false,
+}: SearchHeaderProps) {
   return (
     <section className="w-full py-[2px] px-4 bg-white flex items-center gap-4 absolute top-0">
       <TigSVG />
-      <SearchInput placeholder="위치나 장소 검색" className="grow mr-1" />
+      <SearchInput
+        placeholder={placeholder}
+        className={cn('grow mr-1 ', {
+          'placeholder:text-grey7': result,
+        })}
+      />
     </section>
   );
 }
