05c517a493a1ad643db747197366dde6bcf9455e
Programming-Seungwan <swsj4480@gmail.com>
2024-07-05 17:46:53 +0900

    [Feat] : /reservation-list/review 페이지 컴포넌트 완성

--- src/app/reservation-list/review/[reviewId]/page.tsx
+++ src/app/reservation-list/review/[reviewId]/page.tsx
@@ -1,13 +1,31 @@
 import Header from '@components/all/Header';
-import NavBar from "@components/all/NavBar/NavBar";
-
+import NavBar from '@components/all/NavBar/NavBar';
+import Review from '@components/reservation-list/review/Review';
+import { ReviewProps } from 'types/reservation-list/review/ReservationListReviewPageTypes';
 
 export default function page() {
+  const DUMMYREVIEWDATA: ReviewProps = {
+    companyName: '스카이락볼링장',
+    companyAddress: '서울 서대문구 신촌로 73',
+    eventDate: '05.17 (금)',
+    eventStartTime: '오전 10:00',
+    eventEndTime: '오전 11:00',
+    adultCount: 2,
+    reservationUserName: '김티그',
+    rating: 4,
+    rateContent:
+      '역 근처에 시설도 깔끔하고 좋아요! 신촌 볼링장 하면 꼭 여기로 가요. 직원분들도 친절하고 레일도 많고 최고! 담에 친구들이랑 단체 모임하면 또 갈게요~!',
+  };
+
   return (
-    <div>
-      <Header buttonType="back" title="작성한 리뷰" />
-      This is {'예약내역-작성한 리뷰 보기'} page
+    <>
+      <div id="header-wrapper" className="w-full h-fit absolute top-0">
+        <Header buttonType="back" title="작성한 리뷰" />
+      </div>
+      <main className="pt-[44px] w-full h-fit flex justify-center ">
+        <Review {...DUMMYREVIEWDATA} />
+      </main>
       <NavBar />
-    </div>
+    </>
   );
 }
---
+++ src/components/reservation-list/review/ReviewLowerSection.tsx
@@ -0,0 +1,43 @@
+import ReviewsFilledStar from '@public/svg/reviewFilledStar.svg';
+import ReviewsUnfilledStar from '@public/svg/reviewUnfilledStar.svg';
+
+import { ReviewLowerSectionProps } from 'types/reservation-list/review/ReservationListReviewPageTypes';
+
+export default function ReviewLowerSection({
+  reservationUserName,
+  eventDate,
+  adultCount,
+  youngManCount,
+  kidCount,
+  rating,
+  rateContent,
+}: ReviewLowerSectionProps) {
+  return (
+    <section className="w-sevenEightWidth h-fit flex flex-col gap-y-4">
+      <div className="w-full h-fit flex flex-col items-start gap-y-[6px]">
+        <span className="title3 text-grey7">{reservationUserName}</span>
+        <div className="flex justify-between items-center gap-x-[6px]">
+          <p className="caption2 text-grey4">{eventDate}</p>
+          <p className="caption2 text-grey3">|</p>
+          <p className="caption2 text-grey4">
+            {adultCount && `성인 ${adultCount}명 `}
+            {youngManCount && `청소년 ${youngManCount}명 `}
+            {kidCount && `어린이 ${kidCount}명 `}
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col items-start gap-y-[10px]">
+        <p className="w-full flex">
+          {[1, 2, 3, 4, 5].map((number) =>
+            number <= rating ? (
+              <ReviewsFilledStar key={number} />
+            ) : (
+              <ReviewsUnfilledStar key={number} />
+            )
+          )}
+        </p>
+        <span className="body5 text-grey6">{rateContent}</span>
+      </div>
+    </section>
+  );
+}
