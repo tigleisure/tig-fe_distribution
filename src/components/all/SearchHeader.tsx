08c34de90ed6aefb1a8a52acc719de4f1d97ce1f
eunsu <songess@naver.com>
2024-07-06 16:13:37 +0900

    [Fix] : 아이콘 shrink-0부여, 클릭 시 홈으로 이동

--- src/components/all/SearchHeader.tsx
+++ src/components/all/SearchHeader.tsx
@@ -1,6 +1,7 @@
 import TigSVG from '@public/svg/tig.svg';
 import SearchInput from './SearchInput';
 import { cn } from '@utils/cn';
+import Link from 'next/link';
 
 interface SearchHeaderProps {
   placeholder?: string;
@@ -13,7 +14,9 @@ export default function SearchHeader({
 }: SearchHeaderProps) {
   return (
     <section className="w-full py-[2px] px-4 bg-white flex items-center gap-4 absolute top-0">
-      <TigSVG />
+      <Link href={'/'} className="shrink-0 cursor-pointer">
+        <TigSVG  />
+      </Link>
       <SearchInput
         placeholder={placeholder}
         className={cn('grow mr-1 ', {
