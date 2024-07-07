114e0b9a4e03fc566849654fb8b4d51b72bbcc0b
eunsu <songess@naver.com>
2024-07-08 03:10:13 +0900

    [Feat] : 상세페이지에 필요한 컴포넌트 구현 - DetailInfoCard: 업체 디테일 정보를 보여주는 컴포넌트 - ServicesCard: 업체의 서비스를 보여주는 컴포넌트 - VisitedReviewCard: 방문자 리뷰를 보여주는 컴포넌트 - ResButtonCard: 예약하기 버튼을 보여주는 컴포넌트

---
+++ src/components/detail-page/VisitedReviewCard.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { ReviewLowerSectionProps } from 'types/reservation-list/review/ReservationListReviewPageTypes';
+import StarSVG from '@public/svg/star.svg';
+import RightArrowSVG from '@public/svg/rightArrow.svg';
+import ReviewLowerSection from '@components/reservation-list/review/ReviewLowerSection';
+import { set } from 'date-fns';
+import { useState } from 'react';
+import { cn } from '@utils/cn';
+
+interface VisitedReviewCardProps {
+  AvgRating: number;
+  RatingCount: number;
+  reviewList: ReviewLowerSectionProps[];
+}
+
+export default function VisitedReviewCard({
+  AvgRating,
+  RatingCount,
+  reviewList,
+}: VisitedReviewCardProps) {
+  const [selectedReviewPage, setSelectedReviewPage] = useState(1);
+  return (
+    <section className="flex flex-col w-full px-5 py-[40px] gap-6 pb-[118px]">
+      <div className="flex gap-[10px]">
+        <p className="headline2 text-grey7">방문자 리뷰</p>
+        <div className="flex gap-1 headline2 items-center text-primary_orange1">
+          <StarSVG />
+          <p>{AvgRating}</p>
+          <p>({RatingCount})</p>
+        </div>
+      </div>
+      <div className="w-full gap-[10px] flex flex-col">
+        {reviewList.map((review, index) => (
+          <div
+            key={index + AvgRating}
+            className="w-full rounded-[14px] border border-grey3 py-5 flex justify-center items-center"
+          >
+            <ReviewLowerSection {...review} />
+          </div>
+        ))}
+      </div>
+      <div className="w-[160px] flex self-center relative gap-1 justify-center">
+        {[1, 2, 3].map((number) => (
+          <button
+            className={cn('w-6 h-6 flex justify-center items-center title4 rounded-full', {
+              'text-white bg-grey7': selectedReviewPage === number,
+              'text-grey7 bg-white': selectedReviewPage !== number,
+            })}
+            onClick={() => {
+              setSelectedReviewPage(number);
+            }}
+            key={number * 1000}
+          >
+            {number}
+          </button>
+        ))}
+        <RightArrowSVG className="absolute right-0" onClick={() => {
+              setSelectedReviewPage(3);
+            }}/>
+      </div>
+    </section>
+  );
+}
