25fe8bfb3763bdd933dfd12daad4d6e92e57b0e2
Programming-Seungwan <swsj4480@gmail.com>
2024-07-07 18:20:23 +0900

    [Feat] : 로그아웃 모달 생성
    1. 우리가 hook을 이용하는 방식을 써서 모달을 구현했기 때문에 mypage 컴포넌트가 클라이언트 컴포넌트가 됨
    2. 인터셉팅 & 패러랠 라우팅 이용하면 서버 컴포넌트로 유지는 가능할 거 같음

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,12 +1,21 @@
+'use client';
 import NavBar from '@components/all/NavBar/NavBar';
 import My from '@components/mypage/My';
 import NoneArrowHeader from '@components/all/NoneArrowHeader';
+import Modal from '@components/all/Modal';
 
 export default function page() {
   return (
     <div className="flex flex-col h-full pb-[54px] items-center">
       <NoneArrowHeader title="마이페이지" />
       <My />
+      <Modal
+        size="lg"
+        button1Content="이전으로"
+        button2Content="로그아웃"
+        title="로그아웃 하시겠습니까?"
+        // 추후에 로그아웃 함수 부착
+      />
       <NavBar />
     </div>
   );
