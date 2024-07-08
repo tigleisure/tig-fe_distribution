e61372db19a48f0d714075765134f7d2fbc96f5d
Programming-Seungwan <swsj4480@gmail.com>
2024-07-08 14:32:51 +0900

    [Feat] : reservation status bar 완성
    1. 중간에 실선은 after psuedo element를 이용해서 만들어줌 -> cn() 함수가 아주 유용했음!

--- src/app/payment/before/[companyId]/page.tsx
+++ src/app/payment/before/[companyId]/page.tsx
@@ -1,10 +1,11 @@
 import Header from '@components/all/Header';
-
+import ReservationStageBar from '@components/payment/before/ReservationStageBar';
 
 export default function page() {
   return (
-    <div>
-      <Header buttonType="close" isCenter title="예약확인" bgColor="grey"/>
-    </div>
-  )
+    <main className="w-full h-full bg-grey1">
+      <Header buttonType="close" isCenter title="예약확인" bgColor="grey" />
+      <ReservationStageBar />
+    </main>
+  );
 }
