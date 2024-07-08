c65b97483c78a72a4dfdeca3a64d121d878cb884
Programming-Seungwan <swsj4480@gmail.com>
2024-07-08 15:07:45 +0900

    [Feat] : payment before 1단계 예약 확인 UI 완성

--- src/app/payment/before/[companyId]/page.tsx
+++ src/app/payment/before/[companyId]/page.tsx
@@ -1,11 +1,31 @@
 import Header from '@components/all/Header';
 import ReservationStageBar from '@components/payment/before/ReservationStageBar';
+import BeforeFirstStageCard from '@components/payment/before/BeforeFirstStageCard';
+import FullButton from '@components/all/FullButton';
+
+const DUMMYFIRSTSTAGEDATA = {
+  companyName: '스카이락 볼링장',
+  companyAddress: '서울 서대문구 신촌로 73 케이스퀘어 8층',
+  eventDate: '05.17 (금)',
+  adultCount: 2,
+  eventStartTime: '오전 10:00',
+  eventEndTime: '오전 11:00',
+  stageFirstPrice: 22000,
+};
 
 export default function page() {
   return (
-    <main className="w-full h-full bg-grey1">
+    <main className="w-full h-full flex flex-col items-center bg-grey1">
       <Header buttonType="close" isCenter title="예약확인" bgColor="grey" />
       <ReservationStageBar />
+      <BeforeFirstStageCard {...DUMMYFIRSTSTAGEDATA} />
+      <FullButton
+        size="lg"
+        color="white"
+        bgColor="primary_orange1"
+        content="확인"
+        className="absolute !w-eightNineWidth bottom-[30px]"
+      />
     </main>
   );
 }
--- src/components/reservation-list/HistoryEndItem.tsx
+++ src/components/reservation-list/HistoryEndItem.tsx
@@ -17,7 +17,6 @@ export default function HistoryEndItem({
   reservationId,
   reviewId,
 }: HistoryEndItemProps) {
-  console.log(reviewId);
   return (
     <Link
       href={`/reservation-list/reservation/${reservationId}`}
