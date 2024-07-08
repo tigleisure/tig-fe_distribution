226926b035a48d30b7a263f4e1df4a7d231e474e
eunsu <songess@naver.com>
2024-07-08 20:26:24 +0900

    [Feat] : reservation/game 페이지 구현

--- src/app/reservation/game/[companyId]/page.tsx
+++ src/app/reservation/game/[companyId]/page.tsx
@@ -1,10 +1,24 @@
 import Header from '@components/all/Header';
+import MakeResButtonCard from '@components/reservation/MakeResButtonCard';
+import PriceCard from '@components/reservation/PriceCard';
+import ResDateCard from '@components/reservation/ResDateCard';
+import ResGameCard from '@components/reservation/ResGameCard';
+import ResPeopleCountCard from '@components/reservation/ResPeopleCountCard';
+import ResReservationCard from '@components/reservation/ResReservationCard';
 
+const DUMMYPRICE = '10,000';
 
-export default function page() {
+export default function Page() {
+  
   return (
-    <div>
+    <main className="w-full h-full overflow-y-scroll flex flex-col ">
       <Header buttonType="back" isCenter title="예약하기" />
-    </div>
+      <ResDateCard />
+      <ResGameCard />
+      <ResPeopleCountCard />
+      <ResReservationCard />
+      <PriceCard price={DUMMYPRICE} />
+      <MakeResButtonCard />
+    </main>
   )
 }
