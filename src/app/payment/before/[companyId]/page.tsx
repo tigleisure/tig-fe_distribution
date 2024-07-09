d2823cae91d9a06ea1f084f19d6304e55ed85461
Programming-Seungwan <swsj4480@gmail.com>
2024-07-09 12:22:20 +0900

    [Feat] : paymentInfoStore 작성
    1. 1단계와 2단계로 나뉨

--- src/app/payment/before/[companyId]/page.tsx
+++ src/app/payment/before/[companyId]/page.tsx
@@ -6,25 +6,12 @@ import BeforeSecondStageCard from '@components/payment/before/BeforeSecondStageC
 import FullButton from '@components/all/FullButton';
 import useReservationStage from '@store/reservationStageStore';
 import CouponPage from '@components/payment/before/CouponPage';
-import { useState } from 'react';
+import { useEffect } from 'react';
 import { useIsCouponPageOpen } from '@store/couponStore';
-
-const DUMMYFIRSTSTAGEDATA = {
-  companyName: '스카이락 볼링장',
-  companyAddress: '서울 서대문구 신촌로 73 케이스퀘어 8층',
-  eventDate: '05.17 (금)',
-  adultCount: 2,
-  eventStartTime: '오전 10:00',
-  eventEndTime: '오전 11:00',
-  stageFirstPrice: 22000,
-};
-
-const DUMMYSECONDSTAGEDATA = {
-  userName: '김티그',
-  phoneNumber: null,
-  couponDiscountPrice: 0,
-  defaultPrice: 22000,
-};
+import {
+  usePaymentFirstStage,
+  usePaymentSecondStage,
+} from '@store/paymentInfoStore';
 
 export default function Page() {
   const reservationStageState = useReservationStage(
@@ -33,6 +20,47 @@ export default function Page() {
   const isCouponPageOpen = useIsCouponPageOpen(
     (state) => state.isCouponPageOpen
   );
+
+  const firstStageInfoObject = usePaymentFirstStage(
+    (state) => state.firstStageInfoObject
+  );
+
+  const setFirstStageInfoObject = usePaymentFirstStage(
+    (state) => state.setFirstStageInfoObject
+  );
+
+  const secondStageInfoObject = usePaymentSecondStage(
+    (state) => state.secondStageInfoObject
+  );
+
+  const setSecondStageInfoObject = usePaymentSecondStage(
+    (state) => state.setSecondStageInfoObject
+  );
+
+  useEffect(() => {
+    // 실제로는 현재 페이지 컴포넌트가 로드될 때, 날짜, 성인수, 청소년수, 어린이수, 시작 시간, 종료시간, 가격을 백엔드로부터 받아서 상태 값을 설정해준다.
+    // 일단 임시 데이터를 통해 화면 UI 완성
+    const DUMMYFIRSTSTAGEDATA = {
+      companyName: '스카이락 볼링장',
+      companyAddress: '서울 서대문구 신촌로 73 케이스퀘어 8층',
+      eventDate: '05.17 (금)',
+      adultCount: 2,
+      eventStartTime: '오전 10:00',
+      eventEndTime: '오전 11:00',
+      stageFirstPrice: 22000,
+    };
+
+    setFirstStageInfoObject(DUMMYFIRSTSTAGEDATA);
+
+    const DUMMYSECONDSTAGEDATA = {
+      userName: '김티그',
+      phoneNumber: '',
+      couponDiscountPrice: 0,
+      defaultPrice: 22000,
+    };
+
+    setSecondStageInfoObject(DUMMYSECONDSTAGEDATA);
+  }, []);
 
   return !isCouponPageOpen ? (
     <main className="w-full h-full flex flex-col items-center bg-grey1 pb-[100px] overflow-y-scroll">
@@ -45,11 +73,11 @@ export default function Page() {
       />
       <ReservationStageBar />
       {reservationStageState === 1 && (
-        <BeforeFirstStageCard {...DUMMYFIRSTSTAGEDATA} />
+        <BeforeFirstStageCard {...firstStageInfoObject} />
       )}
 
       {reservationStageState === 2 && (
-        <BeforeSecondStageCard {...DUMMYSECONDSTAGEDATA} />
+        <BeforeSecondStageCard {...secondStageInfoObject} />
       )}
 
       {reservationStageState === 1 && (
---
+++ src/store/paymentInfoStore.ts
@@ -0,0 +1,59 @@
+import { create } from 'zustand';
+
+interface paymentFirstStageInfoProps {
+  companyName: string;
+  companyAddress: string;
+  eventDate: string;
+  adultCount: number;
+  youngManCount?: number;
+  kidCount?: number;
+  eventStartTime: string;
+  eventEndTime: string;
+  stageFirstPrice: number;
+}
+
+interface paymentFirstStageStore {
+  firstStageInfoObject: paymentFirstStageInfoProps;
+  setFirstStageInfoObject: (status: paymentFirstStageInfoProps) => void;
+}
+
+// first stage에서의 정보를 관리
+export const usePaymentFirstStage = create<paymentFirstStageStore>((set) => ({
+  firstStageInfoObject: {
+    companyName: '',
+    companyAddress: '',
+    eventDate: '',
+    adultCount: 0,
+    youngManCount: 0,
+    kidCount: 0,
+    eventStartTime: '',
+    eventEndTime: '',
+    stageFirstPrice: 0,
+  },
+  setFirstStageInfoObject: (status: paymentFirstStageInfoProps) =>
+    set({ firstStageInfoObject: status }),
+}));
+
+interface paymentSecondStageInfoProps {
+  userName: string;
+  phoneNumber: string;
+  couponDiscountPrice: number;
+  defaultPrice: number;
+}
+
+interface paymentSecondStageStore {
+  secondStageInfoObject: paymentSecondStageInfoProps;
+  setSecondStageInfoObject: (status: paymentSecondStageInfoProps) => void;
+}
+
+// second stage에서의 정보를 관리
+export const usePaymentSecondStage = create<paymentSecondStageStore>((set) => ({
+  secondStageInfoObject: {
+    userName: '',
+    phoneNumber: '',
+    couponDiscountPrice: 0,
+    defaultPrice: 0,
+  },
+  setSecondStageInfoObject: (status: paymentSecondStageInfoProps) =>
+    set({ secondStageInfoObject: status }),
+}));
