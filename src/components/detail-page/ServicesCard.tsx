ded937cb2176f6c5be546f1251b1c74198bfbc71
eunsu <songess@naver.com>
2024-07-11 16:32:59 +0900

    [Feat] : detail-page에 IntersectionObserver 적용(주석) - 조금 오작동 함에 따라 주석처리 -> 추후 수정

--- src/components/detail-page/ServicesCard.tsx
+++ src/components/detail-page/ServicesCard.tsx
@@ -1,5 +1,7 @@
+import useIntersectionObserver from '@hooks/useIntersectionObserver';
 import WifiSVG from '@public/svg/wifi.svg';
-import { forwardRef } from 'react';
+import useTab from '@store/tabNumberStore';
+import { forwardRef, useRef } from 'react';
 
 interface ServicesCardProps {
   servicesIcon: string[];
@@ -9,9 +11,12 @@ interface ServicesCardProps {
 // eslint-disable-next-line react/display-name
 export const ServicesCard = forwardRef<HTMLDivElement, ServicesCardProps>(
   ({ servicesIcon, services }, ref) => {
+    // const survicesRef = useRef<HTMLDivElement>(null);
+    const setSelectedTab = useTab((state) => state.setSelectedTab);
+    // useIntersectionObserver(survicesRef, () => setSelectedTab('편의시설'),1);
     return (
-      <section
-        className="flex flex-col gap-6 w-full px-5 py-[40px] border-b border-grey2"
+      <section className="flex flex-col gap-6 w-full px-5 py-[40px] border-b border-grey2" 
+      // ref={survicesRef}
       >
         <p className="headline2 text-grey7">편의 시설 및 서비스</p>
         <div className="w-full flex flex-wrap gap-3" ref={ref}>
@@ -19,7 +24,6 @@ export const ServicesCard = forwardRef<HTMLDivElement, ServicesCardProps>(
             <div
               key={icon + index}
               className="flex items-center w-[calc(50%-0.375rem)] gap-2"
-              
             >
               {icon === 'wifi' && <WifiSVG />}
               <p className="title4 text-grey7">{services[index]}</p>
