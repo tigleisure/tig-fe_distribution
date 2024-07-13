3bf0863fa0f3aa17a1d0dee2d4eaa3fce86d45cb
eunsu <songess@naver.com>
2024-07-13 14:42:45 +0900

    [Feat] : 예약페이지 요청처리를 위한 store 구현

---
+++ src/store/makeReservationInfo.ts
@@ -0,0 +1,53 @@
+import { formatDate } from 'date-fns';
+import { create } from 'zustand';
+
+interface MakeReservationInfoProps {
+  date: string;
+  startTime: string;
+  adultCount: number;
+  teenagerCount: number;
+  kidCount: number;
+  askToManager: string;
+}
+interface MakeGameReservationInfoProps extends MakeReservationInfoProps {
+  gameCount: number;
+}
+interface MakeTimeReservationInfoProps extends MakeReservationInfoProps {
+  endTime: string;
+}
+
+interface GameReservationStore {
+  gameReservationInfo: MakeGameReservationInfoProps;
+  setGameReservationInfo: (info: MakeGameReservationInfoProps) => void;
+}
+
+interface TimeReservationStore {
+  timeReservationInfo: MakeTimeReservationInfoProps;
+  setTimeReservationInfo: (info: MakeTimeReservationInfoProps) => void;
+}
+
+export const useGameReservationStore = create<GameReservationStore>((set) => ({
+  gameReservationInfo: {
+    date: formatDate(new Date(), 'yyyy-MM-dd'),
+    startTime: '',
+    adultCount: 0,
+    teenagerCount: 0,
+    kidCount: 0,
+    askToManager: '',
+    gameCount: 0,
+  },
+  setGameReservationInfo: (info) => set({ gameReservationInfo: info }),
+}));
+
+export const useTimeReservationStore = create<TimeReservationStore>((set) => ({
+  timeReservationInfo: {
+    date: formatDate(new Date(), 'yyyy-MM-dd'),
+    startTime: '',
+    endTime: '',
+    adultCount: 0,
+    teenagerCount: 0,
+    kidCount: 0,
+    askToManager: '',
+  },
+  setTimeReservationInfo: (info) => set({ timeReservationInfo: info }),
+}));
