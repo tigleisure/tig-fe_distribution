5da645b36dbc130e2d7c6e25978e14f98f82b4c3
Programming-Seungwan <swsj4480@gmail.com>
2024-07-13 22:31:20 +0900

    [Feat] : 위시리스트 필터링하는 로직 추가
    1. 전체 위시리스트를 받아오고, 그 다음에 tab 전역 상태를 받아와서 진행하는 로직

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import NavBar from '@components/all/NavBar/NavBar';
 import Tabs from '@components/all/Tabs/Tabs';
 import { allleisureArray } from '@constant/constant';
@@ -7,7 +6,8 @@ import NoneArrowHeader from '@components/all/NoneArrowHeader';
 import { ResultCardProps } from 'types/search/result/searchResult';
 import ResultCard from '@components/search/result/ResultCard';
 import { da } from 'date-fns/locale';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import useTab from '@store/tabNumberStore';
 
 const DUMMYRESULTS: ResultCardProps[] = [
   {
@@ -18,7 +18,7 @@ const DUMMYRESULTS: ResultCardProps[] = [
     reviewCount: 100,
     price: '10,000원',
     gameType: '게임',
-    gameNameType: '볼링',
+    gameNameType: '당구',
     isEvent: true,
     image: '/png/dummyImage.png',
   },
@@ -43,7 +43,7 @@ const DUMMYRESULTS: ResultCardProps[] = [
     reviewCount: 100,
     price: '10,000원',
     gameType: '게임',
-    gameNameType: '볼링',
+    gameNameType: '스크린골프',
     isHeart: true,
     image: '/png/dummyImage.png',
   },
@@ -55,7 +55,7 @@ const DUMMYRESULTS: ResultCardProps[] = [
     reviewCount: 100,
     price: '10,000원',
     gameType: '게임',
-    gameNameType: '볼링',
+    gameNameType: '탁구',
     isEvent: true,
     image: '/png/dummyImage.png',
   },
@@ -67,7 +67,7 @@ const DUMMYRESULTS: ResultCardProps[] = [
     reviewCount: 100,
     price: '10,000원',
     gameType: '게임',
-    gameNameType: '볼링',
+    gameNameType: '테니스',
     image: '/png/dummyImage.png',
   },
   {
@@ -78,7 +78,7 @@ const DUMMYRESULTS: ResultCardProps[] = [
     reviewCount: 100,
     price: '10,000원',
     gameType: '게임',
-    gameNameType: '볼링',
+    gameNameType: '당구',
     isEvent: true,
     isHeart: true,
     image: '/png/dummyImage.png',
@@ -104,7 +104,7 @@ const DUMMYRESULTS: ResultCardProps[] = [
     reviewCount: 100,
     price: '10,000원',
     gameType: '게임',
-    gameNameType: '볼링',
+    gameNameType: '스크린골프',
     isEvent: true,
     isHeart: true,
     image: '/png/dummyImage.png',
@@ -117,7 +117,7 @@ const DUMMYRESULTS: ResultCardProps[] = [
     reviewCount: 100,
     price: '10,000원',
     gameType: '게임',
-    gameNameType: '볼링',
+    gameNameType: '테니스',
     isEvent: true,
     isHeart: true,
     image: '/png/dummyImage.png',
@@ -130,14 +130,137 @@ const DUMMYRESULTS: ResultCardProps[] = [
     reviewCount: 100,
     price: '10,000원',
     gameType: '게임',
-    gameNameType: '볼링',
+    gameNameType: '당구',
     isEvent: true,
     isHeart: true,
+    image: '/png/dummyImage.png',
+  },
+  {
+    clubName: '스카이락볼링장1',
+    clubId: 11,
+    location: '서울특별시 강남구 역삼동',
+    rating: 4.5,
+    reviewCount: 100,
+    price: '10,000원',
+    gameType: '게임',
+    gameNameType: '당구',
+    isEvent: true,
+    image: '/png/dummyImage.png',
+  },
+  {
+    clubName: '스카이락볼링장2',
+    clubId: 12,
+    location:
+      '서울특별시 강남구 역삼동 역삼동 역삼동 역삼동 역삼동 역삼동 역삼동 역삼동 역삼동',
+    rating: 4.5,
+    reviewCount: 100,
+    price: '10,000원',
+    gameType: '게임',
+    gameNameType: '볼링',
+    isHeart: true,
+    image: '/png/dummyImage.png',
+  },
+  {
+    clubName: '스카이락볼링장3',
+    clubId: 13,
+    location: '서울특별시 강남구 역삼동',
+    rating: 4.5,
+    reviewCount: 100,
+    price: '10,000원',
+    gameType: '게임',
+    gameNameType: '스크린골프',
+    isHeart: true,
+    image: '/png/dummyImage.png',
+  },
+  {
+    clubName: '스카이락볼링장4',
+    clubId: 14,
+    location: '서울특별시 강남구 역삼동',
+    rating: 4.5,
+    reviewCount: 100,
+    price: '10,000원',
+    gameType: '게임',
+    gameNameType: '탁구',
+    isEvent: true,
+    image: '/png/dummyImage.png',
+  },
+  {
+    clubName: '스카이락볼링장5',
+    clubId: 15,
+    location: '서울특별시 강남구 역삼동',
+    rating: 4.5,
+    reviewCount: 100,
+    price: '10,000원',
+    gameType: '게임',
+    gameNameType: '테니스',
+    image: '/png/dummyImage.png',
+  },
+  {
+    clubName: '스카이락볼링장1',
+    clubId: 16,
+    location: '서울특별시 강남구 역삼동',
+    rating: 4.5,
+    reviewCount: 100,
+    price: '10,000원',
+    gameType: '게임',
+    gameNameType: '당구',
+    isEvent: true,
+    image: '/png/dummyImage.png',
+  },
+  {
+    clubName: '스카이락볼링장1',
+    clubId: 17,
+    location: '서울특별시 강남구 역삼동',
+    rating: 4.5,
+    reviewCount: 100,
+    price: '10,000원',
+    gameType: '게임',
+    gameNameType: '당구',
+    isEvent: true,
+    image: '/png/dummyImage.png',
+  },
+  {
+    clubName: '스카이락볼링장1',
+    clubId: 18,
+    location: '서울특별시 강남구 역삼동',
+    rating: 4.5,
+    reviewCount: 100,
+    price: '10,000원',
+    gameType: '게임',
+    gameNameType: '당구',
+    isEvent: true,
+    image: '/png/dummyImage.png',
+  },
+  {
+    clubName: '스카이락볼링장1',
+    clubId: 19,
+    location: '서울특별시 강남구 역삼동',
+    rating: 4.5,
+    reviewCount: 100,
+    price: '10,000원',
+    gameType: '게임',
+    gameNameType: '당구',
+    isEvent: true,
+    image: '/png/dummyImage.png',
+  },
+  {
+    clubName: '스카이락볼링장1',
+    clubId: 20,
+    location: '서울특별시 강남구 역삼동',
+    rating: 4.5,
+    reviewCount: 100,
+    price: '10,000원',
+    gameType: '게임',
+    gameNameType: '당구',
+    isEvent: true,
     image: '/png/dummyImage.png',
   },
 ];
 
 export default function Page() {
+  const selectedTab = useTab((state) => state.selectedTab);
+  const [wishList, setWishList] = useState<ResultCardProps[]>([]);
+
   useEffect(() => {
     async function getWishlist() {
       const response = await fetch(
@@ -146,18 +269,18 @@ export default function Page() {
           credentials: 'include',
         }
       );
-
       const data = await response.json();
-
       console.log(data);
+      // setWishList(data);
     }
-
     getWishlist();
+
+    setWishList(DUMMYRESULTS);
   }, []);
 
   const tabArray = allleisureArray;
   return (
-    <div className="flex flex-col h-full mb-[54px] items-center">
+    <div className="flex flex-col h-full pb-[54px] items-center">
       <NoneArrowHeader title="위시리스트" />
       <Tabs
         tabArray={tabArray}
@@ -166,11 +289,65 @@ export default function Page() {
         className="w-fit px-5 top-[68px] border-b-[1px] border-grey2"
       />
 
-      <main className="w-full max-h-wishListMain  absolute top-[120px] mt-5 pb-10 overflow-y-scroll">
-        {DUMMYRESULTS.map((data, index) => (
-          <ResultCard key={index} {...data} />
-        ))}
-      </main>
+      {selectedTab === '전체' && (
+        <main className="w-full max-h-wishListMain absolute top-[120px] mt-5 pb-10 overflow-y-scroll">
+          {wishList.map((data, index) => (
+            <ResultCard key={index} {...data} />
+          ))}
+        </main>
+      )}
+
+      {selectedTab === '당구' && (
+        <main className="w-full max-h-wishListMain  absolute top-[120px] mt-5 pb-10 overflow-y-scroll">
+          {wishList
+            .filter((wishListItem) => wishListItem.gameNameType === '당구')
+            .map((data) => (
+              <ResultCard key={data.clubId} {...data} />
+            ))}
+        </main>
+      )}
+
+      {selectedTab === '볼링' && (
+        <main className="w-full max-h-wishListMain  absolute top-[120px] mt-5 pb-10 overflow-y-scroll">
+          {wishList
+            .filter((wishListItem) => wishListItem.gameNameType === '볼링')
+            .map((data) => (
+              <ResultCard key={data.clubId} {...data} />
+            ))}
+        </main>
+      )}
+
+      {selectedTab === '스크린골프' && (
+        <main className="w-full max-h-wishListMain  absolute top-[120px] mt-5 pb-10 overflow-y-scroll">
+          {wishList
+            .filter(
+              (wishListItem) => wishListItem.gameNameType === '스크린골프'
+            )
+            .map((data) => (
+              <ResultCard key={data.clubId} {...data} />
+            ))}
+        </main>
+      )}
+
+      {selectedTab === '탁구' && (
+        <main className="w-full max-h-wishListMain  absolute top-[120px] mt-5 pb-10 overflow-y-scroll">
+          {wishList
+            .filter((wishListItem) => wishListItem.gameNameType === '탁구')
+            .map((data) => (
+              <ResultCard key={data.clubId} {...data} />
+            ))}
+        </main>
+      )}
+
+      {selectedTab === '테니스' && (
+        <main className="w-full max-h-wishListMain  absolute top-[120px] mt-5 pb-10 overflow-y-scroll">
+          {wishList
+            .filter((wishListItem) => wishListItem.gameNameType === '테니스')
+            .map((data) => (
+              <ResultCard key={data.clubId} {...data} />
+            ))}
+        </main>
+      )}
 
       <NavBar />
     </div>
