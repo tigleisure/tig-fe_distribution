9133e6c8cb3fdcabb7128147cdb67b530aadf109
Programming-Seungwan <swsj4480@gmail.com>
2024-07-15 01:22:07 +0900

    [Feat] : 타입 관련 에러 해결
    1. tanstack query 관련된 것은 아직 not yet

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,6 +1,6 @@
-'use client'
+'use client';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function makeQueryClient() {
   return new QueryClient({
@@ -11,32 +11,34 @@ function makeQueryClient() {
         staleTime: 60 * 1000,
       },
     },
-  })
+  });
 }
 
-let browserQueryClient: QueryClient | undefined = undefined
+let browserQueryClient: QueryClient | undefined = undefined;
 
 function getQueryClient() {
   if (typeof window === 'undefined') {
     // Server일 경우
     // 매번 새로운 queryClient를 만든다.
-    return makeQueryClient()
+    return makeQueryClient();
   } else {
     // Browser일 경우
     // queryClient가 존재하지 않을 경우에만 새로운 queryClient를 만든다.
     // React가 새 Client를 만들게 하기 위해 중요하다.
-    if (!browserQueryClient) browserQueryClient = makeQueryClient()
-    return browserQueryClient
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
   }
 }
 
-export default function Providers({ children }) {
+export default function Providers({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   // NOTE: queryClient를 useState를 사용하여 초기화 하면 안된다.
-  // suspense boundary가 없을 경우 React의 렌더링이 중단될 수도 있고 
+  // suspense boundary가 없을 경우 React의 렌더링이 중단될 수도 있고
   // queryClient 자체를 폐기할 수 도 있다.
-  const queryClient = getQueryClient()
+  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  )
+  );
 }
