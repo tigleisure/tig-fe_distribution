fa745f19adf5ef52dcac2d1279198828ac9947a8
eunsu <songess@naver.com>
2024-07-14 12:23:20 +0900

    [Feat] : 게임 수 카드, 요청사항 카드 추가

---
+++ src/components/reservation/GameCountCard.tsx
@@ -0,0 +1,20 @@
+import ChooseCard from '@components/search/ChooseCard';
+import InfoCard from '@components/search/InfoCard';
+import { ChooseCardProps } from 'types/search/SearchTypes';
+
+const chooseLists: ChooseCardProps[] = [
+  { title: '성인', description: '만 19세 이상' },
+  { title: '청소년', description: '만 14세 ~ 만 18세' },
+  { title: '어린이', description: '만 13세 이하' },
+];
+
+export default function GameCountCard() {
+  return (
+    <section className="w-full flex flex-col p-5 mt-5 border-b border-grey2">
+      <InfoCard number={3} content="인원을 선택해주세요." />
+      <ChooseCard
+        title="게임"
+      />
+    </section>
+  );
+}
