20a513e8dd211b4d0aa7a7d9868226de3c068229
eunsu <songess@naver.com>
2024-07-15 14:21:07 +0900

    [Feat] : 홈 페이지에서 postHome API 연동

--- src/components/home/HomeCard.tsx
+++ src/components/home/HomeCard.tsx
@@ -1,34 +1,36 @@
+import { categoryMapEngToKor } from '@constant/constant';
 import { cn } from '@utils/cn';
 import Image from 'next/image';
 import Link from 'next/link';
-import { type HomeCardProps } from 'types/home/HomeTypes';
+import { Club } from 'types/response/response';
 
 export default function HomeCard({
-  isEventCard = false,
   clubName,
-  subtitle,
-  gameType,
-  image,
+  address,
+  category,
+  imageUrls,
   id,
-}: HomeCardProps) {
+}: Club) {
+  const isEventCard = false;
   return (
     <Link
       className="w-[152px] flex flex-col gap-[6px] shrink-0"
       href={`/detail-page/${id}`}
     >
-      <Image src={image} alt={clubName} width={152} height={152} />
+      {/* 서버 더미 이미지들이 제대로 렌더링되지 못해 로컬 더미이미지 사용*/}
+      <Image src={'/png/dummyImage.png'} alt={clubName} width={152} height={152} />
       <div className="flex gap-[6px] mt-[6px]">
         <p className="title3 text-grey7">{clubName}</p>
-        <p className="body4 text-grey5">{gameType}</p>
+        <p className="body4 text-grey5">{categoryMapEngToKor[category]}</p>
       </div>
       <div
-        className={cn('w-fit', {
+        className={cn('w-fit line-clamp-1', {
           'bg-primary_orange2 text-primary_orange1 title3 flex justify-center items-center px-[8px] py-[6px] rounded-[6px]':
             isEventCard,
           'text-grey6 body4': !isEventCard,
         })}
       >
-        {subtitle}
+        {address}
       </div>
     </Link>
   );
