c08eb354b5880d888753993f55985648269d716f
eunsu <songess@naver.com>
2024-07-15 14:19:54 +0900

    [Feat] : categoryMap 상수화 - 한글 -> 영어, 영어 -> 한글 둘다 구현

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -15,3 +15,19 @@ export const homeleisureArray = [
   '테니스',
 ];
 export const detailArray = ['기본정보', '편의시설', '방문자 리뷰'];
+
+export const categoryMapKorToEng: { [key: string]: string } = {
+  당구: 'POCKET_BALL',
+  볼링: 'BALLING',
+  스크린골프: 'SCREEN_GOLF',
+  탁구: 'TABLE_TENNIS',
+  테니스: 'TENNIS',
+};
+
+export const categoryMapEngToKor: { [key: string]: string } = {
+POCKET_BALL: '당구',
+BALLING: '볼링',
+SCREEN_GOLF: '스크린골프',
+TABLE_TENNIS: '탁구',
+TENNIS: '테니스',
+}
