4d421a9cc59b67f8a9f2cf2e7ccec245601c0fc7
Programming-Seungwan <swsj4480@gmail.com>
2024-07-16 20:10:41 +0900

    [Feat] : 처음 로그인 했을 때에 accessToken을 로컬 스토리지에 넣어둠. 개발환경에서 strict mode로 요청이 2번 전송되어 useRef로 이를 막아줌

--- src/app/login/oauth2/code/kakao/KakaoLoginLogic.tsx
+++ src/app/login/oauth2/code/kakao/KakaoLoginLogic.tsx
@@ -1,8 +1,17 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 
+interface kakaoLoginResponseProp {
+  result: {
+    accessToken: string;
+  };
+  resultCode: number;
+  resultMsg: string;
+}
+
 export default function KakaoLoginLogic() {
+  const loadingRef = useRef<boolean>(true);
   const router = useRouter();
   const data = useSearchParams();
   const authCode = data?.get('code');
@@ -18,23 +27,25 @@ export default function KakaoLoginLogic() {
         );
 
         if (response.ok) {
-          const data = response.json();
-          console.log(data);
+          const data: kakaoLoginResponseProp = await response.json();
+          localStorage.setItem('accessToken', data.result.accessToken);
           router.replace('/');
-          console.log('hi');
         } else {
           throw new Error(
             '인증 코드를 기반으로 로그인 하는 데에 실패했습니다!'
           );
         }
       } catch (error) {
-        alert(error);
+        console.log(error);
       }
     }
 
     if (authCode) {
-      sendAuthCodeToBackend();
+      if (loadingRef.current === true) {
+        sendAuthCodeToBackend();
+        loadingRef.current = false;
+      }
     }
-  }, [authCode]);
+  }, []);
   return <div>This is kakao login 리다이렉트 uri 페이지</div>;
 }
