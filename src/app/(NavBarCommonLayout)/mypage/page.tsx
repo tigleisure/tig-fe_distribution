0561a9d3c84620d2e12a9c0670f528ca241be8a0
Programming-Seungwan <swsj4480@gmail.com>
2024-07-16 21:01:43 +0900

    [Feat]: 로그아웃 버튼을 눌렀을 시, 로컬 스토리지에서의 데이터를 삭제하는 작업
    1. refreshToken을 만료시키는 작업도 적어줄 예정임

--- src/app/(NavBarCommonLayout)/mypage/page.tsx
+++ src/app/(NavBarCommonLayout)/mypage/page.tsx
@@ -6,15 +6,24 @@ import Modal from '@components/all/Modal';
 import { Toaster } from 'react-hot-toast';
 import { useEffect } from 'react';
 import useModal from '@store/modalStore';
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
   const setSelectedIsModalOpen = useModal(
     (state) => state.setSelectedIsModalOpen
   );
+  const router = useRouter();
 
   useEffect(() => {
     return () => setSelectedIsModalOpen(false);
   }, []);
+
+  const handleClickLogoutButton = (): void => {
+    console.log('logout!');
+    localStorage.removeItem('accessToken');
+    // 추후에 refreshToken도 무효화시켜달라는 백엔드 API가 필요
+    router.replace('/');
+  };
 
   return (
     <div className="flex flex-col h-full pb-[54px] items-center">
@@ -25,6 +34,7 @@ export default function Page() {
         button1Content="이전으로"
         button2Content="로그아웃"
         title="로그아웃 하시겠습니까?"
+        secondButtonFunc={handleClickLogoutButton}
       />
 
       {/* <NavBar /> */}
