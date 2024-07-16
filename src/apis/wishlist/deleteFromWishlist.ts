82740b17461ae70a119545453c311ebfe7d759e2
eunsu <songess@naver.com>
2024-07-16 23:50:19 +0900

    [Feat] : 좋아요 낙관적 업데이트 구현

--- src/apis/wishlist/deleteFromWishlist.ts
+++ src/apis/wishlist/deleteFromWishlist.ts
@@ -1,6 +1,7 @@
 import { instance } from '@apis/instance';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { DeleteFromWishListResponse } from 'types/response/response';
+import { ResultCardProps } from 'types/search/result/searchResult';
 
 export const deleteFromWishList = async (
   clubId: number
@@ -12,7 +13,37 @@ export const useDeleteFromWishList = () => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: deleteFromWishList,
-    onSuccess: () => {
+
+    onMutate: async (clubId: number) => {
+      await queryClient.cancelQueries({ queryKey: ['wishlist'] });
+
+      // 먼저 해당 아이템의 업데이트 이전 정보를 저장
+      const prevWishlist = queryClient.getQueryData<{ result: ResultCardProps[]; resultCode: number; resultMsg: string }>(['wishlist']);
+
+      if (!prevWishlist) {
+        return { prevWishlist: { result: [] } };
+      }
+      // 새로운 선택지 데이터로 낙관적 업데이트 실시
+      const newOption = {
+        ...prevWishlist,
+        result: prevWishlist.result.filter(item => item.id !== clubId),
+      };
+      queryClient.setQueryData(['wishlist'], newOption);
+
+      // 실패 시 되돌릴 수 있도록 이전 값을 리턴
+      return { prevWishlist };
+    },
+    // 실패 시 이전 데이터로 복구
+    onError: (error, _, context) => {
+      // 캐시를 저장된 값으로 롤백
+      if (context?.prevWishlist) {
+        queryClient.setQueryData(['wishlist'], context.prevWishlist);
+      }
+      window.console.log('error', error);
+    },
+
+    // 실패 성공 여부에 상관없이 해당 쿼리를 무효화
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ['wishlist'] });
     },
   });
