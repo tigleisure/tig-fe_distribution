b49daf8f70afedc55b50de28ae39c2ae502b4a84
eunsu <songess@naver.com>
2024-07-16 15:32:38 +0900

    [Fix] : 필터링 드랍다운 z-index 조정

--- src/components/search/result/FilterHeader.tsx
+++ src/components/search/result/FilterHeader.tsx
@@ -34,7 +34,7 @@ export default function FilterHeader() {
 
   return (
     <section
-      className="absolute top-[110px] flex justify-end h-[32px] title4 text-grey6 w-full pr-5 cursor-pointer z-10 bg-white"
+      className="absolute top-[110px] flex justify-end h-[32px] title4 text-grey6 w-full pr-5 cursor-pointer z-[100] bg-white"
       ref={ref}
       onClick={() => {
         setIsOpen(true);
