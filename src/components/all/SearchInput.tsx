0a53ce5cd3576739585d999aea7f6a3d27dfa5ff
eunsu <songess@naver.com>
2024-07-17 14:31:21 +0900

    [Feat] : SearchInput 컴포넌트에 forwardRef 적용

--- src/components/all/SearchInput.tsx
+++ src/components/all/SearchInput.tsx
@@ -1,27 +1,32 @@
 import SearchInputSVG from '@public/svg/searchInput.svg';
 import { cn } from '@utils/cn';
+import { forwardRef } from 'react';
 
 interface SearchInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   result?: boolean;
   className?: string;
 }
 
-export default function SearchInput({
-  result = false,
-  className,
-  ...props
-}: SearchInputProps) {
-  return (
-    <div className="bg-grey2 w-full h-[40px] flex items-center gap-[6px] body3 px-[12px] rounded-[50px]">
-      <SearchInputSVG />
-      <input
-        type="text"
-        className={cn('grow bg-grey2', {
-          'placeholder-grey7': result,
-          'placeholder-grey5': !result,
-        },className)}
-        {...props}
-      />
-    </div>
-  );
-}
+// eslint-disable-next-line react/display-name
+export const SearchInput = forwardRef<HTMLInputElement, SearchInputProps>(
+  ({ result = false, className, ...props }, ref) => {
+    return (
+      <div className="bg-grey2 w-full h-[40px] flex items-center gap-[6px] body3 px-[12px] rounded-[50px]">
+        <SearchInputSVG />
+        <input
+          type="text"
+          className={cn(
+            'grow bg-grey2',
+            {
+              'placeholder-grey7': result,
+              'placeholder-grey5': !result,
+            },
+            className
+          )}
+          {...props}
+          ref={ref}
+        />
+      </div>
+    );
+  }
+);
