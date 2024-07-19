7d0c9e60e451cca25445fb8e72010090247aad57
Programming-Seungwan <swsj4480@gmail.com>
2024-07-19 11:30:57 +0900

    [Fix] : /payment/before 페이지 포맷 수정 1. 은수가 만들어준 날짜를 7.19 (금) 형태로 바꿔주는 함수 formatReservationShowingDate 함수 작성 2. 시간만 12:00 형식으로 만들어주는 extractOnlyTime 함수 작성

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -5,3 +5,24 @@ export const formatDate = (date: Date): string => {
 
   return `${year}.${month}.${day}`;
 };
+
+export const formatReservationShowingDate = (dt: string): string => {
+  const date = new Date(dt);
+
+  const month = (date.getMonth() + 1).toString();
+  const day = date.getDate().toString();
+  const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+  const dayOfWeek = daysOfWeek[date.getDay()];
+
+  return `${month}.${day} (${dayOfWeek})`;
+};
+
+// 시간의 양식은 '2024-07-19T14:00:00'임
+export const extractOnlyTime = (dt: string): string => {
+  const date = new Date(dt);
+
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+
+  return `${hours}:${minutes}`;
+};
