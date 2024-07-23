80514445dedcccced59fd66339b659142672c7c1
eunsu <songess@naver.com>
2024-07-24 00:11:56 +0900

    [Fix] : API 오타 수정

--- src/apis/djemalsvpdlwl/getTBCReservation.ts
+++ src/apis/djemalsvpdlwl/getTBCReservation.ts
@@ -1,6 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
 import { ReservationItemProps } from 'types/reservation-list/ReservationListPageTypes';
-import { instance } from '@apis/instance';
 
 export interface TBCReservationListResponse {
   result: ReservationItemProps[];
@@ -12,7 +11,7 @@ export const getTBCReservationList =
   async (): Promise<TBCReservationListResponse> => {
     // TBC reservation list
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN}/api/v1/reservation/TBC`
+      `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN}/api/v1/reservation/tbc`
     );
 
     if (!response.ok) {
@@ -25,7 +24,7 @@ export const getTBCReservationList =
 
 export const useGetTBCReservationList = () => {
   return useQuery({
-    queryKey: ['TBCReservationList'],
+    queryKey: ['TBCReservation'],
     queryFn: getTBCReservationList,
   });
 };
