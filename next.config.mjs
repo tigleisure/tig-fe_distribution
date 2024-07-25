e2ccd7c18a5b3a396324212f97ab109a9edd52a9
eunsu <songess@naver.com>
2024-07-25 21:00:43 +0900

    [Chore] : next.config.mjs에 이미지 도메인 추가 - next/image는 외부도메인을 사용할 때 domains 옵션을 추가해야함

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['t1.kakaocdn.net'],
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
