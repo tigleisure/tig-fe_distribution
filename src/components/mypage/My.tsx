18285cc0225b521a21059f12a5e832d9db0f3830
eunsu <songess@naver.com>
2024-07-25 21:01:10 +0900

    [Feat] : 마이페이지에서 카카오에서 제공하는 프로필 이미지로 변경

--- src/components/mypage/My.tsx
+++ src/components/mypage/My.tsx
@@ -3,13 +3,25 @@ import MyProfileDefaultImage from '@public/svg/myProfileDefaultImage.svg';
 import ProfileInformation from './ProfileInformation';
 import ToastUI from './ToastUI';
 import useModal from '@store/modalStore';
+import { useGetUserInfo } from '@apis/mypage/getUserInfo';
+import Image from 'next/image';
+import TigLoadingPage from '@components/all/TigLoadingPage';
 
 export default function My() {
   const setIsOpen = useModal((state) => state.setSelectedIsModalOpen);
+  const { data, isSuccess } = useGetUserInfo();
+  if (!isSuccess) return <TigLoadingPage />;
   return (
     <div className="w-full flex flex-col items-center absolute top-[68px] pt-5">
       <div className="w-eightNineWidth mypageWidth h-fit flex flex-col items-center gap-y-[30px] mb-[30px]">
-        <MyProfileDefaultImage />
+        {/* <MyProfileDefaultImage /> */}
+        <Image
+          src={data.result.profileImage}
+          alt="프로필 이미지"
+          className="rounded-[50%]"
+          width={80}
+          height={80}
+        />
         <ProfileInformation />
       </div>
       <div className="w-full border-[1px] border-grey2 mb-[30px]"></div>
