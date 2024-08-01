51194b11b40bd79812cf0e8adb04e253f3f3d3d1
eunsu <songess@naver.com>
2024-08-02 00:20:37 +0900

    [Feat] : game 예약시에도 적용 - 저번에 적용하지 않았던 선택된 날짜 반영하는 로직, 예약 가능한 시간대 반영하는 로직 추가

--- src/app/reservation/game/[companyId]/page.tsx
+++ src/app/reservation/game/[companyId]/page.tsx
@@ -14,6 +14,10 @@ import {
 } from '@store/makeReservationInfo';
 import { useGetClubResInfo } from '@apis/reservation/getClubResInfo';
 import { Toaster } from 'react-hot-toast';
+import { useSearchParams } from 'next/navigation';
+import { addHours, formatDate } from 'date-fns';
+import { timeToMinutes } from '@utils/formatDate';
+import { useSelectedDate } from '@store/selectedDateStore';
 
 export default function Page({ params }: { params: { companyId: string } }) {
   const { data, isSuccess } = useGetClubResInfo(params.companyId);
@@ -21,6 +25,12 @@ export default function Page({ params }: { params: { companyId: string } }) {
   const [endTime, setEndTime] = useState('');
   const [clubName, setClubName] = useState('');
   const [address, setAddress] = useState('');
+  const searchParam = useSearchParams();
+  const selectedDate = useSelectedDate((state) => state.selectedDate);
+  const setSelectedDate = useSelectedDate((state) => state.setSelectedDate);
+  const gameReservationInfo = useGameReservationStore(
+    (state) => state.gameReservationInfo
+  );
   const setGameReservationInfo = useGameReservationStore(
     (state) => state.setGameReservationInfo
   );
@@ -31,10 +41,30 @@ export default function Page({ params }: { params: { companyId: string } }) {
       setEndTime(data?.result.businessHours.slice(8, 13) || '20:00');
       setClubName(data?.result.clubName || '');
       setAddress(data?.result.address || '');
+      setGameReservationInfo({
+        ...gameReservationInfo,
+        date: searchParam.get('date') || '',
+      });
+      setSelectedDate(searchParam.get('date') || '');
     }
     // 언마운트될 때 다시 초기화
     return () => setGameReservationInfo(gameReservationInfoInitialState);
   }, [data]);
+
+  useEffect(() => {
+    const now = formatDate(addHours(new Date(), 1), 'HH:00');
+    if (
+      timeToMinutes(data?.result.businessHours?.slice(0, 5) || '10:00') <
+        timeToMinutes(now) &&
+      selectedDate.slice(0, 10) === formatDate(new Date(), 'yyyy-MM-dd')
+    ) {
+      setStartTime(now);
+    } else {
+      setStartTime(data?.result.businessHours.slice(0, 5) || '10:00');
+    }
+    setEndTime(data?.result.businessHours.slice(8, 13) || '20:00');
+  }, [selectedDate]);
+
   return (
     <main className="w-full h-full overflow-y-scroll flex flex-col ">
       <Header buttonType="back" isCenter title="예약하기" />
@@ -43,7 +73,11 @@ export default function Page({ params }: { params: { companyId: string } }) {
       <ResPeopleCountCard />
       <GameCountCard />
       <RequestCard />
-      <MakeResButtonCard clubName={clubName} address={address} clubStartTime={startTime}/>
+      <MakeResButtonCard
+        clubName={clubName}
+        address={address}
+        clubStartTime={startTime}
+      />
       <Toaster position="bottom-center" containerStyle={{ bottom: '90px' }} />
     </main>
   );
