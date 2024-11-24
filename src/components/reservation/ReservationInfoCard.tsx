ee0d18367bfe4c0648b284a03e92626e1523fb9c
tigleisure <2290072@ewhain.net>
2024-11-24 12:13:02 +0000

    [Docs] : 멘트 수정

--- src/components/reservation/ReservationInfoCard.tsx
+++ src/components/reservation/ReservationInfoCard.tsx
@@ -6,7 +6,7 @@ export default function ReservationInfoCard() {
       <div className="flex gap-3 items-center">
         <InfoSvg />
         <p className="title3 text-grey7">
-          1명이 여러 게임을 치는 경우에도 총 게임 수를 선택해주세요.
+          1명이 여러 게임을 하는 경우에도 총 게임 수를 선택해주세요.
         </p>
       </div>
       <p className="body4 text-grey5 ml-[32px]">
