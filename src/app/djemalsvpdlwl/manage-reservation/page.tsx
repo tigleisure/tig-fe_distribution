5cb7e6c6b999e3d963f8ac52b41b695079fe5539
tigleisure <2290072@ewhain.net>
2024-11-27 09:23:56 +0000

    [Feat] : 예약내역이 없을때 안내 문구 추가

--- src/app/djemalsvpdlwl/manage-reservation/page.tsx
+++ src/app/djemalsvpdlwl/manage-reservation/page.tsx
@@ -6,6 +6,9 @@ export default function Page() {
   const { data, isSuccess } = useGetTBCReservationList();
   return (
     <main className="w-full h-full flex flex-col py-[20px] items-center overflow-y-scroll gap-[10px]">
+      {isSuccess && data.result.length === 0 && (
+        <div className="text-grey7 title2">예약 내역이 없습니다.</div>
+      )}
       {isSuccess &&
         data.result.map((reservationInfo) => (
           <HistoryInAdminItem
