12fc51d3882acec01a4da72d3bc5c66ee6bb6fc0
tigleisure <2290072@ewhain.net>
2024-11-27 09:29:54 +0000

    [Feat] : 클라이언트 컴포넌트로 변경

--- src/apis/djemalsvpdlwl/getFeedback.ts
+++ src/apis/djemalsvpdlwl/getFeedback.ts
@@ -5,3 +5,10 @@ import { FeedbackListResponse } from 'types/response/response';
 export const getFeedbacklist = async (): Promise<FeedbackListResponse> => {
   return instance.get(`/api/v1/feedback`);
 };
+
+export const useGetFeedbacklist = () => {
+  return useQuery({
+    queryKey: ['feedback'],
+    queryFn: getFeedbacklist,
+  });
+};
--- src/app/djemalsvpdlwl/show-feedback/page.tsx
+++ src/app/djemalsvpdlwl/show-feedback/page.tsx
@@ -1,17 +1,26 @@
-import { getFeedbacklist } from '@apis/djemalsvpdlwl/getFeedback';
-import { Feedback } from 'types/response/response';
+'use client';
+import {
+  getFeedbacklist,
+  useGetFeedbacklist,
+} from '@apis/djemalsvpdlwl/getFeedback';
+import { useEffect, useState } from 'react';
+import { Feedback, FeedbackListResponse } from 'types/response/response';
 
-export default async function Page() {
-  const feedback = await getFeedbacklist();
-  console.log(feedback);
+export default function Page() {
+  const { data, isSuccess } = useGetFeedbacklist();
+
   return (
-    <div className='w-full flex flex-col gap-5 py-5 px-10 '>
-      {feedback.result.map((item: Feedback, idx) => (
-        <div key={idx} className='flex flex-col gap-4 w-full items-center border border-grey3 rounded-md shadow-sm p-4 overflow-y-scroll'>
-          <div className='title2'>{item.memberName}</div>
-          <div className='body2'>{item.message}</div>
-        </div>
-      ))}
+    <div className="w-full flex flex-col gap-5 py-5 px-10 ">
+      {isSuccess &&
+        data.result.map((item: Feedback, idx) => (
+          <div
+            key={idx}
+            className="flex flex-col gap-4 w-full items-center border border-grey3 rounded-md shadow-sm p-4 overflow-y-scroll"
+          >
+            <div className="title2">{item.memberName}</div>
+            <div className="body2">{item.message}</div>
+          </div>
+        ))}
     </div>
   );
 }
