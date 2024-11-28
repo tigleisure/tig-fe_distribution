a924b6601d267d6690c386226677d11d629ef916
tigleisure <2290072@ewhain.net>
2024-11-28 07:21:54 +0000

    [Feat] : 과거의 시간으로 예약할 수 없도록 수정 - 과거의 시간으로 예약페이지에 접근할 수 없음

--- src/components/reservation/ResDateCard.tsx
+++ src/components/reservation/ResDateCard.tsx
@@ -1,7 +1,17 @@
-import InfoCard from "@components/all/InfoCard";
-import Calender from "@components/search/Calender";
+import InfoCard from '@components/all/InfoCard';
+import Calender from '@components/search/Calender';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function ResDateCard() {
+  const searchParam = useSearchParams();
+  const router = useRouter();
+  useEffect(() => {
+    const dateParam = searchParam.get('date');
+    if (dateParam && dateParam < new Date().toISOString().split('T')[0]) {
+      router.back();
+    }
+  }, []);
   return (
     <section className="w-full flex flex-col gap-5 p-5 mt-5 border-b border-grey2 pt-[68px]">
       <InfoCard number={1} content="날짜를 선택해주세요." />
